1f9c7398789f5aea46f29b6c1bafb95d477e1894
MeexReay <meexreay@gmail.com>
2025-03-21 23:03:04 +0300

    fix colors littlebit

--- sys/terminal.js
+++ sys/terminal.js
@@ -100,16 +100,16 @@ function stripColors(content) {
 }
 
 function makeColorCodesPrintable(input) {
-    return input.replace(/\$#([0-9a-fA-F]{6})/g, '$$##$1')
-                .replace(/\$([A-Z_]+)--/g, '$$#$1--')
-                .replace(/\$reset/g, '$$#reset');
+    return input.replace(/\$#([0-9a-fA-F]{6})/g, '$##$1')
+                .replace(/\$([A-Z_]+)--/g, '$#$1--')
+                .replace(/\$reset/g, '$#reset');
 }
 
 function convertColorCodes(input) {
     return input.replace(/\$#([0-9a-fA-F]{6})/g, '<span style="color: #$1">')
                 .replace(/\$##([0-9a-fA-F]{6})/g, '$#$1')
                 .replace(/\$([A-Z_]+)--/g, '<span style="color: --$1">')
-                .replace(/\$#([A-Z_]+)--/g, '$$$1--')
+                .replace(/\$#([A-Z_]+)--/g, '$$1--')
                 .replace(/\$reset/g, '</span>')
                 .replace(/\$#reset/g, '$reset');
 }
