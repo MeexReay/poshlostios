d73fe415b109aa424d0a9d082c020f2a7c82eaa0
MeexReay <meexreay@gmail.com>
2025-03-21 18:30:11 +0300

    cursor fix

--- sys/system.js
+++ sys/system.js
@@ -32,6 +32,7 @@ async function readLine(on_key=(key, ctrl, alt, shift, content, pos) => [content
                     content = content.slice(0, pos - 1) + content.slice(pos)
                     pos -= 1
                     setTerminal(start_terminal.substring(0, terminal_text.length-length) + content)
+                    let cursor = getCursor()
                     setCursor(cursor[0]-1, cursor[1])
                     updateTerminalWOCursor()
                 }
