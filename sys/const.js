52a2adf5a35d248f59c7dcbf3972f1ead4fbdbfe
MeexReay <meexreay@gmail.com>
2025-09-20 21:23:20 +0300

    fix default ppm-app url

--- sys/const.js
+++ sys/const.js
@@ -1,5 +1,5 @@
 const MAX_STORAGE = 5120 * 1024 // Limit of local storage size, 5120 KB by default
-const APP_REPOSITORY = "https://github.com/MeexReay/ppm-app/blob/main" // Repository url to download apps from
+const APP_REPOSITORY = "https://raw.githubusercontent.com/MeexReay/ppm-app/refs/heads/main" // Repository url to download apps from
 const DEFAULT_APPS = [
     "posh", // required
     "hello",
